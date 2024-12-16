8189751e47cb6e7e8a167a168997245fdbeb9ff4
Junyoung Lee <tim000421@gmail.com>
2024-12-16 15:46:55 -0800

    initialize

---
+++ src/main/java/com/kilo/klio/KlioApplication.java
@@ -0,0 +1,13 @@
+package com.kilo.klio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KlioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KlioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kilo/klio/KlioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kilo.klio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KlioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
